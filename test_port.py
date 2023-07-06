11df659e0e7e78970c623ee6ed4984fb16ba4b32
Lutenruto <pereiravazluis@gmail.com>
2023-07-06 14:40:55 +0200

    Debug test_port

--- test_port.py
+++ test_port.py
@@ -1,11 +1,9 @@
-import os
 import requests
 from requests.exceptions import Timeout
 from concurrent.futures import ThreadPoolExecutor
 
 base_url = 'http://{ip}:{port}/ping'
 ip_addresses = ['10.33.2.123']  # Ajoutez ici les adresses IP à tester
-
 stop_execution = False
 
 
@@ -20,14 +18,14 @@ def test_ping_ports(ip_address, start_port, end_port):
             if response.status_code == 200 and response.text.lower() == 'pong':
                 print('Réponse 200 "pong" reçue sur le port :', port)
                 stop_execution = True
-                return
+                return port
         except Timeout:
             pass
 
 
 def main():
     num_workers = 100
-    port_range = 65535 - 1024 + 1
+    port_range = 4096 - 1024 + 1
     ports_per_worker = port_range // num_workers
 
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
@@ -39,13 +37,17 @@ def main():
                 end_port = start_port + ports_per_worker - 1
 
                 if i == num_workers - 1:
-                    end_port = 65535
+                    end_port = 4096
 
                 future = executor.submit(test_ping_ports, ip_address, start_port, end_port)
                 futures.append(future)
 
         for future in futures:
-            future.result()
+            port = future.result()
+            if port is not None:
+                return port
+
+    return None
 
 
 if __name__ == '__main__':
